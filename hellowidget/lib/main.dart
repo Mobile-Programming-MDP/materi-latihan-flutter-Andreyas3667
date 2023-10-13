ecbdb210c5a73c316728b31fc8edfb5d3dcee825
Andreyas <andreyasming@gmail.com>
2023-10-13 18:38:06 +0700

    Update

--- hellowidget/lib/main.dart
+++ hellowidget/lib/main.dart
@@ -24,6 +24,21 @@ class FirstScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Andreyas"),
+        actions: <Widget>
+            //Icon Di Kanan Appbar
+            [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.black,
+              ))
+        ],
+        leading: IconButton
+            //Icon Di Kiri Appbar
+            (
+                onPressed: () {},
+                icon: const Icon(Icons.home, color: Colors.black)),
       ),
       body: const Center(
         child: Text("Hallo Andreyas"),
