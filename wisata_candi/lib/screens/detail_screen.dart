d670ec76fcbd949325a0e023ef9fbc44522cfa59
Andreyas3667 <andreyasming@gmail.com>
2023-10-23 00:32:05 +0700

    Update detail_screen.dart

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -9,6 +9,7 @@ class DetailScreen extends StatelessWidget {
     return  Scaffold(
       body: Column(
         children: [
+          //DetailHeader
           Stack(
             children: [
               //Image Utama
@@ -27,11 +28,61 @@ class DetailScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: Colors.blueAccent[100]?.withOpacity(0.8),
                   shape: BoxShape.circle),
+                  //BocDecoration
                   child: IconButton(onPressed: () {},icon: const Icon(Icons.arrow_back)),
               ),
               )
             ],
           )
+        //DetailInfo
+        padding:const EdgeInsets.symmetric(horizontal:16),
+        child:Column(
+          crossAxisAligment:crossAxisAligment.start,
+          children[
+            SizedBox(height:16,),
+            //Nama Candi & Tombol Favorit
+            Row(
+              mainAxisAligment:mainAxisAligment.spaceBetween,
+              children:[
+                Text(candi.name,style:textStyle(
+                  fontSize:20,fontWeight:fontWeight.bold,
+                ),
+              ),
+                IconButton(onPressed:(){},icon:Icon(Icons.favorite_border),
+                )
+              ],
+            ),
+            //Lokasi, Dibangun, Tipe
+            SizedBox(height:16),
+            Row(children:[
+              Icon(Icons.place,color:Colors.red,),
+              SizedBox(width:8,),
+              SizedBox(width:70,
+                child:Text('Lokasi',style:TextStyle(
+                    fontWeight:FontWeight.bold),),),
+                    Text(': ${candi.location}'),
+            ],),
+            Row(children:[
+               Icon(Icons.calendar_month,color:Colors.red,),
+              SizedBox(width:8,),
+              SizedBox(width:70,
+                child:Text('Dibangun',style:TextStyle(
+                    fontWeight:FontWeight.bold),),),
+                    Text(': ${candi.built}'),
+            ],),
+            Row(children:[
+              Icon(Icons.house,color:Colors.red,),
+              SizedBox(width:8,),
+              SizedBox(width:70,
+                child:Text('Tipe',style:TextStyle(
+                    fontWeight:FontWeight.bold),),),
+                    Text(': ${candi.type}'),
+            ],),
+            SizedBox(height:16,),
+            Divider(color:Colors.deepPurple.shade100,),
+            SizedBox(height:16,),
+          ]
+        )
         ],
       ),
     );
